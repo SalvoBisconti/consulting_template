2876c3bd08b0223e2fe1accca97454ab6a952da8
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-06-22 23:35:30 +0200

    init

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="">
+      <h2> HERO</h2>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="">
+      <h2> Navbar</h2>
+    </div>
+  );
+};
+
+export default Navbar;
