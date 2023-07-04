8990026a59ab9fc5f14eb6640eb65eae362697f9
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-07-04 23:58:46 +0200

    footer completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import AboutSection from "@/components/aboutSection";
 import CarouselSection from "@/components/carouselSection";
 import DictionarySection from "@/components/dictionarySection";
 import FaqSection from "@/components/faqSection";
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import InfoSection from "@/components/infoSection";
 import Navbar from "@/components/navbar";
@@ -18,6 +19,7 @@ export default function Home() {
       <ServiceSection />
       <FaqSection />
       <InfoSection />
+      <Footer />
     </main>
   );
 }
