b4b0408306d87407ba837d5e0a156eb029029a8d
SalvoBisconti <salvatorebisconti98@gmail.com>
2023-07-22 16:53:25 +0200

    fixed video autoplay battery low

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,24 +1,26 @@
+import { useEffect, useRef } from "react";
 const Hero = () => {
-  const videoElement: any = document.getElementById("video");
-  videoElement
-    .play()
-    .then(() => {})
-    .catch((error: any) => {
-      videoElement.setAttribute("controls", "controls");
-    });
+  const videoCurrent = useRef<HTMLVideoElement | null>(null);
+  useEffect(() => {
+    if (videoCurrent.current) {
+      videoCurrent.current.play().catch(() => {
+        if (videoCurrent.current) videoCurrent.current.controls = true;
+      });
+    }
+  }, []);
 
   return (
     <div className="relative top-0 h-screen w-screen flex " id="home">
       <div className="z-10 absolute top-0 right-0 w-full h-full bg-[#0000004d]"></div>
       <div className="w-full h-full absolute bg-fixed">
         <video
-          id="video"
+          ref={videoCurrent}
           preload="auto"
           autoPlay
           loop
           muted
           playsInline
-          className="absolute w-full h-full object-cover bg-fixed object-right"
+          className="absolute w-full h-full object-cover bg-fixed object-[70%] "
         >
           <source
             src="https://my-firstandgeneral-bucket1.s3.eu-central-1.amazonaws.com/hero.mp4"
